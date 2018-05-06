0417f809e04666002339b5109731e66adf33bfaf
Dom <d.celiano@yahoo.com>
2018-05-06 20:38:27 +0100

    Added/extended ssp debugging. 13.56MHz

--- fpga/fpga_hf.v
+++ fpga/fpga_hf.v
@@ -111,8 +111,7 @@ wire [2:0] mod_type = hi_simulate_mod_type;
 // ISO14443-A support for the Proxmark III
 // Gerhard de Koning Gans, April 2008
 //-----------------------------------------------------------------------------
-//wire osc_clk = ck_1356meg; //change this to change the clock source.
-wire osc_clk = pck_clkdiv;
+wire osc_clk = ck_1356meg; //change this to change the clock source.
 assign adc_clk = osc_clk;
 
 
@@ -196,15 +195,15 @@ begin
 	else // look for the steepest edges (slopes)
 	begin
 		if (adc_d_filtered > 0)
-			begin
-				if (adc_d_filtered > rx_mod_falling_edge_max)
-					rx_mod_falling_edge_max <= adc_d_filtered;
-			end
+		begin
+			if (adc_d_filtered > rx_mod_falling_edge_max)
+				rx_mod_falling_edge_max <= adc_d_filtered;
+		end
 		else
-			begin
-				if (adc_d_filtered < rx_mod_rising_edge_max)
-					rx_mod_rising_edge_max <= adc_d_filtered;
-			end
+		begin
+			if (adc_d_filtered < rx_mod_rising_edge_max)
+				rx_mod_rising_edge_max <= adc_d_filtered;
+		end
 	end
 
 end
